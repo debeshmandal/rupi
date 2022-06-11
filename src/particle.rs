f46fed5ad66e87b248edecc4c30978ad317f17f4
Debesh Mandal <debesh.mandal@hotmail.com>
2022-06-11 11:15:40 +0100

    Add particle::Particle.{pos, vel, force}

--- src/particle.rs
+++ src/particle.rs
@@ -1,15 +1,18 @@
 #[derive(Debug)]
 pub struct Particle {
-  pub mass: f32,
-  /*pub pos: [f32, 3],
-  pub vel: [f32, 3],
-  pub force: [f32, 3], */
+  mass: f32,
+  pos: [f32; 3],
+  vel: [f32; 3],
+  force: [f32; 3],
 }
 
 impl Particle {
   pub fn new() -> Self {
     Particle{
-      mass: 1.0
+      mass: 1.0,
+      pos: [0.0, 0.0, 0.0],
+      vel: [1.0, 0.0, 0.0],
+      force: [0.0, 0.0, 0.0],
     }
   }
 }
