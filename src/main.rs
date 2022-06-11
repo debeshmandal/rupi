8bb1c1f1c7637068f01ec16e3b31834773fa7288
Debesh Mandal <debesh.mandal@hotmail.com>
2022-06-11 12:57:18 +0100

    Move simple simulation test to test

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,28 @@ mod system;
 mod integrator;
 
 fn main() {
-    let mut p = particle::Particle::new();
+  println!("Welcome to rupi!")
+}
+
+#[cfg(test)]
+mod tests {
+
+  use crate::{
+    particle::Particle,
+    system::System,
+    integrator::Integrator,
+  };
+
+  #[test]
+  fn simple_simulation() {
+    let mut p = Particle::new();
+    let mut s = System::new([10.0, 10.0, 10.0]);
+    let i = Integrator::new(0.1);
+
     p.pos[0] += 1.0;
-    println!("Hello, world!");
-    println!("{:?}", p);
-    let mut s = system::System::new([10.0, 10.0, 10.0]);
     s.particles.push(p);
-    println!("{:?}", s);
-    let i = integrator::Integrator::new(0.1);
-    println!("{:?}", i);
     i.run(&mut s, 91);
-    println!("{:?}", s);
+
+    assert_eq!(s.particles[0].pos[1], 0.0)
+  }
 }
