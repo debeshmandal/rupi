38d8514dfaf8cb9b5c9e0b9e2a7f27a7a345d696
Debesh Mandal <debesh.mandal@hotmail.com>
2022-06-11 11:04:21 +0100

    Add files for integrator, particle, and system
    particle.rs contains the Particle struct which is used for managing particle positions, velocities and forces

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod particle;
+
 fn main() {
+    particle::Particle{mass: 1.0};
     println!("Hello, world!");
 }
---
+++ src/particle.rs
@@ -0,0 +1,6 @@
+pub struct Particle {
+  pub mass: f32,
+  /*pub pos: [f32, 3],
+  pub vel: [f32, 3],
+  pub force: [f32, 3],*/
+}
